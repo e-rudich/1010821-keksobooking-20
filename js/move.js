a21276d10cd87adc98a250500c97363f19fdd1cc
Elena <e.d.rudich@gmail.com>
2020-07-06 20:22:28 +0300

    исправление крайних координат

--- js/move.js
+++ js/move.js
@@ -5,8 +5,8 @@
   var mapBorder = {
     top: window.data.locationYMin - window.form.pinActiveHeight,
     bottom: window.data.locationYMax - window.form.pinActiveHeight,
-    left: window.data.loactionXMin - window.form.pinWidth / 2,
-    right: window.data.locationXMax - window.form.pinWidth / 2
+    left: window.data.loactionXMin - Math.ceil(window.form.pinWidth / 2),
+    right: window.data.locationXMax - Math.ceil(window.form.pinWidth / 2)
   };
 
   // Перемещение пина
@@ -29,27 +29,22 @@
       };
 
       // Функция проверки пределов перемещения
-      var checkBorders = function () {
-        if (window.form.mainPin.offsetLeft + shift.x < mapBorder.left) {
-          window.form.mainPin.style.left = mapBorder.left + 'px';
-        }
+      if (window.form.mainPin.offsetLeft - shift.x < mapBorder.left) {
+        window.form.mainPin.style.left = mapBorder.left + 'px';
+      } else if (window.form.mainPin.offsetLeft - shift.x > mapBorder.right) {
+        window.form.mainPin.style.left = mapBorder.right + 'px';
+      } else {
+        window.form.mainPin.style.left = (window.form.mainPin.offsetLeft - shift.x) + 'px';
+      }
 
-        if (window.form.mainPin.offsetLeft - shift.x > mapBorder.right) {
-          window.form.mainPin.style.left = mapBorder.right + 'px';
-        }
+      if (window.form.mainPin.offsetTop - shift.y < mapBorder.top) {
+        window.form.mainPin.style.top = mapBorder.top + 'px';
+      } else if (window.form.mainPin.offsetTop - shift.y > mapBorder.bottom) {
+        window.form.mainPin.style.top = mapBorder.bottom + 'px';
+      } else {
+        window.form.mainPin.style.top = (window.form.mainPin.offsetTop - shift.y) + 'px';
+      }
 
-        if (window.form.mainPin.offsetTop + shift.y < mapBorder.top) {
-          window.form.mainPin.style.top = mapBorder.top + 'px';
-        }
-
-        if (window.form.mainPin.offsetTop + shift.y > mapBorder.bottom) {
-          window.form.mainPin.style.top = mapBorder.bottom + 'px';
-        }
-      };
-
-      window.form.mainPin.style.top = (window.form.mainPin.offsetTop - shift.y) + 'px';
-      window.form.mainPin.style.left = (window.form.mainPin.offsetLeft - shift.x) + 'px';
-      checkBorders();
       window.form.insertDefaultAddressEnabled();
     };
 
